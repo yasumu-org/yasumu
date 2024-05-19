b4138ab2582c34f7016bd5c0368208b9aeac6108
twlite <46562212+twlite@users.noreply.github.com>
2024-05-19 19:41:04 +0545

    fix: remove old files

--- src/hooks/useListener.tsx
+++
@@ -1,9 +0,0 @@
-import { EventName, EventCallback, listen } from '@tauri-apps/api/event';
-import { useEffect } from 'react';
-
-export function useListener<P>(event: EventName, handler: EventCallback<P>) {
-  useEffect(() => {
-    const cleanup = listen(event, handler);
-    return () => void cleanup.then((c) => c());
-  }, []);
-}
