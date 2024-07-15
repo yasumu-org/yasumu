bd2f12380073adc31dbbdd9928afda188472517a
twlite <46562212+twlite@users.noreply.github.com>
2024-07-15 20:23:05 +0545

    chore: additional deps

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_websocket::init())
         .plugin(tauri_plugin_http::init())
         .setup(|app| {
             let main_window = app.get_webview_window("main").unwrap();
