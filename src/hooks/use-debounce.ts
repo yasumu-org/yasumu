ff130bfb9ec45f9bbf9c04c856e7b3141be26bfd
samir-byte <kiranpoudel28@gmail.com>
2024-08-13 09:07:33 +0545

    fix: focus disappears issue

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    // Update debounced value after delay
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Cancel the timeout if value or delay changes
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
