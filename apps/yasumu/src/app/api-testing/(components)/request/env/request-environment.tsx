664caba3ee9421a5a984c4e74aa49164399f0fbf
twlite <46562212+twlite@users.noreply.github.com>
2024-09-17 02:15:43 +0545

    feat: basic env templating

--- apps/yasumu/src/app/api-testing/(components)/request/env/request-environment.tsx
+++ apps/yasumu/src/app/api-testing/(components)/request/env/request-environment.tsx
@@ -1,16 +1,27 @@
+'use client';
+
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { useEnvironment } from '@/stores/application/environment.store';
 
 export function RequestEnvironment() {
+  const { environments, selected, selectById } = useEnvironment();
+
   return (
-    <Select>
+    <Select
+      value={selected?.id}
+      onValueChange={(value) => {
+        selectById(value);
+      }}
+    >
       <SelectTrigger className="w-[130px] h-8 rounded-none px-2 py-1 select-none border-none bg-inherit">
         <SelectValue placeholder="Environment" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="production">Production</SelectItem>
-        <SelectItem value="staging">Staging</SelectItem>
-        <SelectItem value="nightly">Nightly</SelectItem>
-        <SelectItem value="local">Local</SelectItem>
+        {environments.map((env) => (
+          <SelectItem value={env.id} key={env.id}>
+            {env.name}
+          </SelectItem>
+        ))}
       </SelectContent>
     </Select>
   );
