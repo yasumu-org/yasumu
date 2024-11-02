89b2ee146f1c5b239d29d2e871dc87050dda7e65
twlite <46562212+twlite@users.noreply.github.com>
2024-11-02 20:46:30 +0545

    fix: make the text unselectable

--- apps/yasumu/src/app/page.tsx
+++ apps/yasumu/src/app/page.tsx
@@ -3,7 +3,7 @@ import YasumuLogo from '@/components/assets/YasumuLogo';
 export default function Home() {
   return (
     <main className="w-full h-screen relative grid place-items-center">
-      <div className="flex flex-col items-center justify-center opacity-10 -z-10 absolute">
+      <div className="flex flex-col items-center justify-center opacity-10 -z-10 absolute select-none">
         <YasumuLogo className="dark:invert-0 invert" height={256} width={256} />
         <h1 className="font-bold text-8xl">Yasumu</h1>
       </div>
