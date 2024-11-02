988596384bf7ebd2a236c6c7857c7d6b4cc16220
twlite <46562212+twlite@users.noreply.github.com>
2024-11-02 18:27:47 +0545

    fix: update tailwind config

--- apps/yasumu/tailwind.config.ts
+++ apps/yasumu/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: ['selector'],
+  safelist: ['dark'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
