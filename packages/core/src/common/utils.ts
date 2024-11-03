bd3617ae38d17041761d2ad0324569ad8936602e
twlite <46562212+twlite@users.noreply.github.com>
2024-11-04 01:02:10 +0545

    fix(core): use crypto for random number

--- packages/core/src/common/utils.ts
+++ packages/core/src/common/utils.ts
@@ -7,7 +7,7 @@ export function generateId() {
     return crypto.randomUUID();
   } catch {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-      const r = (Math.random() * 16) | 0;
+      const r = crypto.getRandomValues(new Uint8Array(1))[0] % 16;
       const v = c === 'x' ? r : (r & 0x3) | 0x8;
       return v.toString(16);
     });
