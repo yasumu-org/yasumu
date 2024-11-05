83c836fb76be7b9de68bc656dbabf8345654ddfb
DevNitro5 <anishchhetri196@gmail.com>
2024-11-05 15:06:34 +0000

    refactor: remove header

--- apps/yasumu/src/components/sidebars/environment-sidebar.tsx
+++ apps/yasumu/src/components/sidebars/environment-sidebar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarHeader } from '../ui/sidebar';
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent } from '../ui/sidebar';
 import { FolderInput, Trash } from 'lucide-react';
 import Link from 'next/link';
 
@@ -8,13 +8,6 @@ const data = ['Local', 'Testing'];
 export function EnvironmentSidebar(props: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="none" {...props}>
-      <SidebarHeader className="gap-3.5 border-b-2 p-4">
-        <div className="w-full items-center space-y-1">
-          <h2 className="text-base font-medium text-foreground">Environments</h2>
-          <p className="text-xs font-medium text-foreground/75">Manage environment of the workspace</p>
-        </div>
-      </SidebarHeader>
-
       <SidebarContent className="zw-scrollbar">
         <SidebarGroup>
           <SidebarGroupContent>
@@ -34,7 +27,7 @@ export function EnvironmentSidebar(props: React.ComponentProps<typeof Sidebar>) 
                     pathname: '/environment',
                     query: { env },
                   }}
-                  className="flex flex-col items-start gap-2 whitespace-nowrap border-b p-2 text-sm leading-tight last:border-b-0 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground"
+                  className="flex flex-col items-start gap-2 whitespace-nowrap p-2 text-sm leading-tight hover:bg-sidebar-accent hover:text-sidebar-accent-foreground"
                 >
                   <span className="text-foreground">{env}</span>
                 </Link>
