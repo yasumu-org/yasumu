902fda66391a85099d78cec0558e840906fb63db
twlite <46562212+twlite@users.noreply.github.com>
2024-11-09 10:19:00 +0545

    test: setup tests

---
+++ packages/testing/__test__/yasumu.spec.ts
@@ -0,0 +1,9 @@
+import { describe, test, expect } from 'vitest';
+import { yasumu } from '../src/index';
+import { Yasumu } from '@yasumu/core';
+
+describe('Yasumu', () => {
+  test('should be Yasumu instance', () => {
+    expect(yasumu).toBeInstanceOf(Yasumu);
+  });
+});
