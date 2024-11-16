69d7f7d87bde9b8dfb49b59ec57aad4f49db4765
vr-varad <varadgupta21@gmail.com>
2024-11-16 15:54:47 +0530

    Testing Mock FileSystem
    Signed-off-by: vr-varad <varadgupta21@gmail.com>

--- packages/testing/__test__/fileSystem.spec.ts
+++ packages/testing/__test__/fileSystem.spec.ts
@@ -4,6 +4,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 
 const testFilePath = path.join(__dirname, 'testFile.txt');
+const testDirPath = path.join(__dirname, 'testDir');
 
 describe('FileSystem', () => {
   beforeAll(async () => {
@@ -19,9 +20,9 @@ describe('FileSystem', () => {
     expect(result.size).toBe(27);
   });
 
-  test('should have a copyFile method', () => {
+  test('should have a copyFile method', async () => {
     const copyPath = path.join(__dirname, 'copyFile.txt');
-    yasumu.fs.copyFile(testFilePath, copyPath);
+    await yasumu.fs.copyFile(testFilePath, copyPath);
     expect(fs.readFile(copyPath, { encoding: 'utf-8' })).resolves.toBe('Initial content for testing');
     yasumu.fs.remove(copyPath);
   });
@@ -30,12 +31,24 @@ describe('FileSystem', () => {
     expect(yasumu.fs.exists(testFilePath)).resolves.toBe(true);
   });
 
-  test('should have a mkdir method', () => {
-    expect(yasumu.fs.mkdir).toBeDefined();
+  test('should have a mkdir method', async () => {
+    await yasumu.fs.mkdir(testDirPath);
+    const exists = await yasumu.fs.exists(testDirPath);
+    expect(exists).toBe(true);
+
+    const stats = await yasumu.fs.lstat(testDirPath);
+    expect(stats.isDirectory).toBe(true);
   });
 
-  test('should have a readDir method', () => {
-    expect(yasumu.fs.readDir).toBeDefined();
+  test('should have a readDir method', async () => {
+    const file1 = path.join(testDirPath, 'file1.txt');
+    await fs.writeFile(file1, 'File 1 content');
+    const entries = await yasumu.fs.readDir(testDirPath);
+    const entryNames = entries.map((entry) => entry.name);
+
+    expect(entries).toBeDefined();
+    expect(Array.isArray(entries)).toBe(true);
+    expect(entryNames).toContain('file1.txt');
   });
 
   test('should have a readFile method', async () => {
@@ -44,7 +57,7 @@ describe('FileSystem', () => {
   });
 
   test('should have a remove method', async () => {
-    yasumu.fs.remove(testFilePath);
+    await yasumu.fs.remove(testFilePath);
     const result = await yasumu.fs.exists(testFilePath);
     expect(result).toBe(false);
   });
